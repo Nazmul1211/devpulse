daeacb1432b67a8a742dc99d5fbaf4d27fb39970
Nazmul Hasan <mnhs1211@gmail.com>
2026-06-30 16:10:51 +0600

    altered and updated the issues and users table column varchar size for the email, role, type and status columns

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,5 @@
 import { Pool } from "pg";
 import config from "../config";
-import type { Request, Response } from "express";
 
 export const pool = new Pool({
   connectionString: config.connection_string,
@@ -12,9 +11,9 @@ export const initDB = async () => {
             CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             name TEXT,
-            email VARCHAR(25) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
             password TEXT NOT NULL,
-            role VARCHAR(15) DEFAULT 'contributor',
+            role VARCHAR(50) DEFAULT 'contributor',
             created_at TIMESTAMP DEFAULT NOW(),
             updated_at TIMESTAMP DEFAULT NOW()
             )
@@ -25,8 +24,8 @@ export const initDB = async () => {
             id SERIAL PRIMARY KEY,
             title VARCHAR(100) NOT NULL,
             description TEXT NOT NULL,
-            type VARCHAR(15) NOT NULL CHECK (type IN ('bug', 'feature_request')),
-            status VARCHAR(15) DEFAULT 'open' CHECK (status IN ('open','in_progress','resolved')),
+            type VARCHAR(50) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+            status VARCHAR(50) DEFAULT 'open' CHECK (status IN ('open','in_progress','resolved')),
             reporter_id INT REFERENCES users(id) ON DELETE CASCADE,
             created_at TIMESTAMP DEFAULT NOW(),
             updated_at TIMESTAMP DEFAULT NOW(),
